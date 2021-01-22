436d96a72b0d271f00a0181efab906cc4b378431
faysalbisaws <faysalbiswas0@gmail.com.com>
2021-01-22 19:46:15 +0600

    first check

---
+++ problem solve.js
@@ -0,0 +1,18 @@
+// https://github.com/faysalbisaws/assignment-js
+
+function kilometerToMeter(meter){
+    var meter = meter*1000;
+    return meter;
+}
+
+var megaFriends = ["hasan","khalil","rahamat","moniruzzaman"];
+    var max = megaFriends[0];
+for (var i = 0; i < megaFriends.length;i++){
+    var element = megaFriends[i];
+   
+    if ( element > max){
+        max = element;
+       
+    }
+   
+}
